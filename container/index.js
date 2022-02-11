a11ceae12801aac9e768915435caf9578bc78ed3
teday <tedi.suryadi77@gmail.com>
2022-02-11 13:14:19 +0700

    share wa

--- container/index.js
+++ container/index.js
@@ -1,16 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useRouter } from "next/router";
-import { Container, Card, CardGroup, Row, Col, Accordion, Button } from 'react-bootstrap'
+import { Container, Card, Modal, Row, Col, Accordion, Button, FormControl, Alert } from 'react-bootstrap'
 import Data from '../data.json'
 import convertRupiah from 'rupiah-format'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function ListProduk() {
 
+  const [openModal, setOpenModal] = useState(false);
+  const [showNotif, setShowNotif] = useState(false);
+  const [NoWa, setNoWa] = useState(null);
+  const [produk, setProduk] = useState(null);
+
   const router = useRouter();
+
+  const handleClose = () => {
+    setNoWa(null)
+    setOpenModal(false)
+    setShowNotif(false)
+  };
+
+  const handleShow = (produk) => {
+    setProduk(produk)
+    setOpenModal(true)
+    setShowNotif(false)
+  };
+
+  const shareProduk = () => {
+    if(NoWa == null){
+      setShowNotif(true)
+    }else{
+      const phoneNumber = NoWa.toString();
+      const phone_number = `+62${phoneNumber.substring(
+        phoneNumber.indexOf("8")
+      )}`;
+      window.open(`https://wa.me/${phone_number}?text=Produk%20%3A%20${produk.nama_produk}%7F%0ADescription%20%3A%20${produk.desc_produk}%7F%0AHarga%20%3A%20${convertRupiah.convert(produk.harga_produk)}`)
+    }
+  }
 
   return (
     <Container fluid style={{ marginTop: '5px' }}>
+      <Modal show={openModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>No wa tujuan</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          { showNotif ?  
+            <Alert variant="danger">
+              No wa harus di isi
+            </Alert> : null
+          }
+          <FormControl type="number" placeholder="08123456789" onChange={ (e) => setNoWa(e.target.value) }/>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Tutup
+          </Button>
+          <Button variant="primary" onClick={shareProduk}>
+            Share Produk
+          </Button>
+        </Modal.Footer>
+      </Modal>
       {Data.produk.filter( title => router.pathname == "/" ? title : router.pathname == "/"+title.title ? title : null ).map((data, i) => {
         return (
           <Accordion defaultActiveKey={0} key={i}>
@@ -33,7 +83,7 @@ export default function ListProduk() {
                             </Card.Text>
                           </Card.Body>
                           <Card.Footer className="d-grid gap-2">
-                            <Button variant="success">Share Produk</Button>
+                            <Button variant="success" onClick={ () => handleShow(produk) }>Share Produk</Button>
                           </Card.Footer>
                         </Card>
                       </Col>
